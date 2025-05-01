9d0ae2d5002beb4da58cae9b380ecdb28e405fb0
Hariom Sharan <hariomsharan2771@gmail.com>
2025-05-01 17:25:10 +0530

    update

--- DishDiscover-Backend/app.js
+++ DishDiscover-Backend/app.js
@@ -1,48 +1,51 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors')
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var cors = require("cors");
 // db configuration
-require('./model/config');
+require("./model/config");
 
-var usersRouter = require('./routes/userRouter');
-var recipeRouter = require('./routes/recipeRouter');
+var usersRouter = require("./routes/userRouter");
+var recipeRouter = require("./routes/recipeRouter");
 
 var app = express();
 
-app.use(cors({
-  origin: "https://dish-discover-lyart.vercel.app/",
-}));
+app.use(
+  cors({
+    origin: "https://dish-discover-lyart.vercel.app",
+    credentials: true,
+  })
+);
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/users', usersRouter);
-app.use('/users/recipe', recipeRouter);
+app.use("/users", usersRouter);
+app.use("/users/recipe", recipeRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
